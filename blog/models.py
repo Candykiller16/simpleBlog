033db466bf6b134043f16bc51631dfce5c93cf56
Candykiller16 <Candykiller16@mail.ru>
2022-01-15 19:05:25 +0300

    5 commit with changes in DB and artcile_details.html

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255) # title Поста, текстовый формат с максимальной длиной 255 символов
+    title_tag = models.CharField(max_length=255, default='My blog') # отвечает за название вкладки
     author = models.ForeignKey(User, on_delete=models.CASCADE) # внешний ключ для таблицы User, с каскадным удалением
     body = models.TextField()
 
