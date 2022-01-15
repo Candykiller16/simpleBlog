20540d0fe35e6ccb186a04a08dc618a2cfc3ce7b
Candykiller16 <Candykiller16@mail.ru>
2022-01-15 16:33:19 +0300

    first creation of blog

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=255) # title Поста, текстовый формат с максимальной длиной 255 символов
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # внешний ключ для таблицы User, с каскадным удалением
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author) # т.к. author это объект, необходимо преобразовать его в строку
+
