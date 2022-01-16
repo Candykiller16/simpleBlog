bbd9929d1b8dfc3fbba3e0d91820d9dffb0e996f
Candykiller16 <Candykiller16@mail.ru>
2022-01-16 13:20:33 +0300

    added functional for delete post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # ListView встроенный класс в django, позволяющий просматривать все записи в нашем посте, т.е. делается QuerySet в БД
 # для отображения всех записей из таблицы, а DetailView для просмотра только одной записи из одной таблицы
 # ListView  - all Posts on our page
@@ -10,12 +10,15 @@ from .models import Post
 
 from .forms import PostForm, EditForm
 
+from django.urls import reverse_lazy
+
 # def home(request):
 #     return render(request, 'home.html', {})
 
 class HomeView(ListView): # view для отображения данных на главной странице
     model = Post # прописываем модель, данные мз которой мы будем отображать
     template_name = 'home.html' # html страничка на которой это будет отображаться
+    ordering = ['-id'] # посты будут отображаться в обратном порядке сверху вниз
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -33,4 +36,11 @@ class UpdatePostView(UpdateView):
     form_class = EditForm
     template_name = 'update_post.html'
     # Нельзя использовать form_class и fields одновременно.
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
+    # в models.py был прописан get_absolute_url для перенаправления после созд. и удаления поста
+    # но это не работает при удалении поста, для этого надо указать success_url
 
